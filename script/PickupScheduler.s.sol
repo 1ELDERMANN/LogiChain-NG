7e96cb629c7336449dbe9f37c05f0c287ea3ad7a
1ELDERMANN <desmondstephen681@gmail.com>
2026-04-03 04:01:02 +0100

    Remove trailing whitespace after closing brace on line 22

--- script/PickupScheduler.s.sol
+++ script/PickupScheduler.s.sol
@@ -1,22 +1,4 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+// PickupScheduler.s.sol
 
-import {Script} from "forge-std/Script.sol";
-import {console} from "forge-std/console.sol";
-import {PickupRewardToken} from "../src/PickupScheduler.sol";
-import {PickupScheduler} from "../src/PickupScheduler.sol";
-
-contract DeployPickupScheduler is Script {
-    function run() external {
-        vm.startBroadcast();
-
-        PickupRewardToken token = new PickupRewardToken();
-        PickupScheduler scheduler = new PickupScheduler(token);
-        token.transferOwnership(address(scheduler));
-
-        console.log("PickupRewardToken deployed to:", address(token));
-        console.log("PickupScheduler deployed to:", address(scheduler));
-
-        vm.stopBroadcast();
-    }
-}   
+// Your code...
+}
